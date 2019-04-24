93d171ecb216e1545466a76a88aff3961c4e56ce
MacTavishAO <ataberkozen123@gmail.com>
2019-04-24 17:52:11 +0300

    [Lang] Initial Turkish Translation

--- tg_bot/modules/language.py
+++ tg_bot/modules/language.py
@@ -16,7 +16,7 @@ def locale(bot, update, args):
     if len(args) > 0:
         locale = args[0].lower()
         if locale in list_locales:
-            if locale in  ('en', 'ru', 'ua', 'es'):
+            if locale in  ('en', 'ru', 'ua', 'es', 'tr'):
                 switch_to_locale(chat.id, locale)
                 update.message.reply_text(tld(chat.id, 'Switched to {} successfully!').format(list_locales[locale]))
             else:
