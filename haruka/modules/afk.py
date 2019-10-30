105293536bbb94b83249e3b52e601fb5116934fc
Gegham Zakaryan <zakaryan.2004@outlook.com>
2019-10-30 15:33:46 +0400

    afk: Fix spam when a afk user is mentioned several times in one message
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- haruka/modules/afk.py
+++ haruka/modules/afk.py
@@ -52,16 +52,27 @@ def reply_afk(bot: Bot, update: Update):
     message = update.effective_message  # type: Optional[Message]
     if message.entities and message.parse_entities([MessageEntity.TEXT_MENTION, MessageEntity.MENTION]):
         entities = message.parse_entities([MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
+
+        chk_users = []
         for ent in entities:
             if ent.type == MessageEntity.TEXT_MENTION:
                 user_id = ent.user.id
                 fst_name = ent.user.first_name
+
+                if user_id in chk_users:
+                    return
+                chk_users.append(user_id)
 
             elif ent.type == MessageEntity.MENTION:
                 user_id = get_user_id(message.text[ent.offset:ent.offset + ent.length])
                 if not user_id:
                     # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
                     return
+
+                if user_id in chk_users:
+                    return
+                chk_users.append(user_id)
+                
                 try:
                     chat = bot.get_chat(user_id)
                 except BadRequest:
